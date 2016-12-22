594ec78cb97f14ea81880e892351a580745b6fb9
Simon Kim <spamhere@kaoma.net>
2016-12-22 17:28:11 +0800

    Add mock API /api/zones

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
-
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -35,42 +33,54 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
-});
+// MOCK API
+const apiFiles = {
+  '/api/zones': path.join(__dirname, 'data/zones.json')
+}
 
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
+function setupAPI( apiPath, dataPath) {
+
+    app.get(apiPath, function(req, res) {
+      fs.readFile(dataPath, function(err, data) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        res.json(JSON.parse(data));
+      });
     });
-  });
-});
 
+    app.post(apiPath, function(req, res) {
+      fs.readFile(dataPath, function(err, data) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        var comments = JSON.parse(data);
+        // NOTE: In a real implementation, we would likely rely on a database or
+        // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+        // treat Date.now() as unique-enough for our purposes.
+        var newComment = {
+          id: Date.now(),
+          author: req.body.author,
+          text: req.body.text,
+        };
+        comments.push(newComment);
+        fs.writeFile(dataPath, JSON.stringify(comments, null, 4), function(err) {
+          if (err) {
+            console.error(err);
+            process.exit(1);
+          }
+          res.json(comments);
+        });
+      });
+    });
+
+}
+
+Object.keys(apiFiles).forEach(function(key) {
+  setupAPI(key, apiFiles[key]);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
