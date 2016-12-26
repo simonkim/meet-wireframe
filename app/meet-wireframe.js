c36e3e10d4c57da6e0d48f81177f92324ac1f74a
Simon Kim <spamhere@kaoma.net>
2016-12-26 09:24:29 +0800

    Remove unnecessary comments

--- app/meet-wireframe.js
+++ app/meet-wireframe.js
@@ -13,20 +13,6 @@ const Zone = require('./zone')
 const User = require('./user')
 const Mates = require('./mates')
 
-/* AJAX example:
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-*/
-
 class WFTop extends React.Component {
 
   /** 
